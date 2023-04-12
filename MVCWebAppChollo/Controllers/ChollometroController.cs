405ae936de0b0c44d78c07b7ade47e4c669b6021
andreiggar <andreiggar@gmail.com>
2023-04-12 13:22:56 +0200

    Add project files.

---
+++ MVCWebAppChollo/Controllers/ChollometroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebAppChollo.Models;
+using MVCWebAppChollo.Repositories;
+
+namespace MVCWebAppChollo.Controllers
+{
+    public class ChollometroController : Controller
+    {
+        private RepositoryChollos repo;
+
+        public ChollometroController(RepositoryChollos repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Chollo> chollos = await this.repo.GetChollosAsync();
+            return View(chollos);
+        }
+    }
+}
---
+++ MVCWebAppChollo/Repositories/RepositoryChollos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCWebAppChollo.Data;
+using MVCWebAppChollo.Models;
+
+namespace MVCWebAppChollo.Repositories
+{
+    public class RepositoryChollos
+    {
+        private DataContext context;
+
+        public RepositoryChollos(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Chollo>> GetChollosAsync()
+        {
+            var consulta = from datos in this.context.Chollos
+                           orderby datos.Fecha descending
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+    }
+}
